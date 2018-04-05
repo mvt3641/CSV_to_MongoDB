e0c3bcb0b11f387d7b9c19f2f5bd156c004a2bf9
Matt <mvt3641@gmail.com>
2018-04-05 18:07:17 -0400

    added template

---
+++ template.js
@@ -0,0 +1,34 @@
+var json2csv = require('json2csv');
+
+exports.get = function(req,res){
+
+
+var fields =[
+'date',
+'month',
+'aerostat_SN',
+'site_ID',
+'time',
+'system_ST',
+'flight',
+'reason',
+'launches',
+'recoveries',
+'flights',
+'tension',
+'winds_Aloft',
+'pitch',
+'helium',
+'ballonet',
+'ground_Winds',
+ 'barometer'
+];
+
+var csv = json2csv({data: '', fields:field});
+
+res.set("Content-Disposition", "attachment;filename=sitrep.csv");
+res.set("Content-Type", "application/octet-stream");
+
+res.send(csv);
+
+};
