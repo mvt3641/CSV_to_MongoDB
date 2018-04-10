44868ba3261b1c94828867a6a02b21609114d093
Matt <mvt3641@gmail.com>
2018-04-09 22:54:04 -0400

    working files upload

--- upload.js
+++ upload.js
@@ -19,20 +19,20 @@ exports.post = function (req, res) {
 		 data['_id'] = new mongoose.Types.ObjectId();
 
 		 flightdata.push(data);
-			 if(flightdata.length === 50){
-				 flight.create(flightdata, function(err, documents) {
-				 if (err) throw err;
-				 flightdata =[];
-				 console.log(`${flightdata.length}files added to database`);
+			 // if(flightdata.length === 50){
+				 // flight.create(flightdata, function(err, documents) {
+				 // if (err) throw err;
+				 // console.log(`${flightdata.length} files added to database`);
+				 // flightdata =[];
 			 })
 			  // flightdata =[];
-		 }
-	 })
+		 // }
+	 // })
 	 .on("end", function(){
-		 // flight.create(flightdata, function(err, documents) {
-			// if (err) throw err;
-
+		 flight.create(flightdata, function(err, documents) {
+			if (err) throw err;
+				console.log(flightdata.length + ' files have been successfully uploaded.');
 			res.send(flightdata.length + ' files have been successfully uploaded.');
-		 // });
+		  });
 	 });
 };
